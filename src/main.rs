269d7095296d22d7b462444a78e1c9d1af5adff9
Duane Littleton <dlittleton87@gmail.com>
2024-12-01 22:39:27 -0500

    Add tracing support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use tracing::{debug, info};
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    tracing_subscriber::fmt()
+        .with_max_level(args.verbosity)
+        .init();
+
     println!("Hello, world!");
+    debug!("Hello from debug!");
+    info!("Hello from info!");
 }
