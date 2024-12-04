9892dda387a14fedf250cdf7011b4ab2c9909af1
Duane Littleton <dlittleton87@gmail.com>
2024-12-03 22:40:15 -0500

    Day 1, part 2

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use aoc2024::input::get_all_numbers;
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
 }
 
-fn part1(input: &str) -> String {
+fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
     let mut first = Vec::new();
     let mut second = Vec::new();
 
@@ -14,6 +16,12 @@ fn part1(input: &str) -> String {
         second.push(*nums.last().unwrap());
     });
 
+    (first, second)
+}
+
+fn part1(input: &str) -> String {
+    let (mut first, mut second) = parse(input);
+
     first.sort();
     second.sort();
 
@@ -21,6 +29,23 @@ fn part1(input: &str) -> String {
         .into_iter()
         .zip(second)
         .map(|(a, b)| (a - b).abs())
+        .sum();
+
+    format!("{}", total)
+}
+
+fn part2(input: &str) -> String {
+    let (first, second) = parse(input);
+
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+
+    second.into_iter().for_each(|k| {
+        *counts.entry(k).or_insert(0) += 1;
+    });
+
+    let total: i32 = first
+        .into_iter()
+        .map(|v| v * counts.get(&v).unwrap_or(&0))
         .sum();
 
     format!("{}", total)
@@ -44,5 +69,12 @@ mod tests {
         let contents = SAMPLE.trim_start();
         let result = part1(contents);
         assert_eq!("11", result);
+    }
+
+    #[test]
+    fn test_part2() {
+        let contents = SAMPLE.trim_start();
+        let result = part2(contents);
+        assert_eq!("31", result);
     }
 }
