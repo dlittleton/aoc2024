b5a39f019f1c6686595ed415f6fdd143eb08292d
Duane Littleton <dlittleton87@gmail.com>
2024-12-04 20:26:39 -0500

    Day 2, part 2

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -2,7 +2,15 @@ use aoc2024::{input::get_all_numbers, sample};
 use tracing::debug;
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
+}
+
+fn check_diffs(diffs: &[i32]) -> bool {
+    let sign = diffs[0].signum();
+
+    diffs
+        .into_iter()
+        .all(|d| d.signum() == sign && d.abs() >= 1 && d.abs() <= 3)
 }
 
 fn is_safe(levels: &[i32]) -> bool {
@@ -11,12 +19,7 @@ fn is_safe(levels: &[i32]) -> bool {
         diffs.push(levels[i] - levels[i - 1]);
     }
 
-    let sign = diffs[0].signum();
-
-    let result = diffs
-        .into_iter()
-        .all(|d| d.signum() == sign && d.abs() >= 1 && d.abs() <= 3);
-
+    let result = check_diffs(&diffs);
     debug!("{:?} is {}", levels, result);
     result
 }
@@ -31,6 +34,40 @@ fn part1(input: &str) -> String {
     count.to_string()
 }
 
+fn is_safe2(levels: &[i32]) -> bool {
+    let mut diffs = Vec::new();
+
+    for skip in 0..levels.len() {
+        let mut diff = Vec::new();
+        let mut last = None;
+
+        for i in 0..levels.len() {
+            if i == skip {
+                continue;
+            }
+
+            if let Some(x) = last {
+                diff.push(levels[i] - x)
+            }
+
+            last = Some(levels[i]);
+        }
+        diffs.push(diff);
+    }
+
+    diffs.into_iter().any(|d| check_diffs(&d))
+}
+
+fn part2(input: &str) -> String {
+    let count = input
+        .split('\n')
+        .map(|s| get_all_numbers::<i32>(s))
+        .filter(|v| is_safe2(&v))
+        .count();
+
+    count.to_string()
+}
+
 sample! {
     r"
 7 6 4 2 1
@@ -39,5 +76,6 @@ sample! {
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9",
-    part1 = "2"
+    part1 = "2",
+    part2 = "4"
 }
