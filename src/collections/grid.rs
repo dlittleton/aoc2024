a5311300be26a6c150c2549d09896190c406c8a1
Duane Littleton <dlittleton87@gmail.com>
2024-12-10 22:16:03 -0500

    Add Position for finding grid neighbors

--- src/collections/grid.rs
+++ src/collections/grid.rs
@@ -3,6 +3,48 @@ pub struct Grid<T> {
     values: Vec<Vec<T>>,
 }
 
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+fn get_direction_delta(dir: Direction) -> (isize, isize) {
+    match dir {
+        Direction::North => (-1, 0),
+        Direction::East => (0, 1),
+        Direction::South => (1, 0),
+        Direction::West => (0, -1),
+    }
+}
+
+pub struct Position<'a, T> {
+    row: usize,
+    col: usize,
+    grid: &'a Grid<T>,
+}
+
+impl<T> Position<'_, T> {
+    pub fn row(&self) -> usize {
+        self.row
+    }
+
+    pub fn col(&self) -> usize {
+        self.col
+    }
+
+    pub fn value(&self) -> &T {
+        self.grid.get(self.row, self.col)
+    }
+
+    pub fn get_neighbor(&self, dir: Direction) -> Option<Self> {
+        let (dr, dc) = get_direction_delta(dir);
+        self.grid
+            .position(self.row as isize + dr, self.col as isize + dc)
+    }
+}
+
 impl<T, I> FromIterator<I> for Grid<T>
 where
     I: Iterator<Item = T>,
@@ -23,26 +65,28 @@ where
 }
 
 impl<T> Grid<T> {
-    pub fn get(&self, i: usize, j: usize) -> &T {
-        &self.values[i][j]
+    pub fn get(&self, row: usize, col: usize) -> &T {
+        &self.values[row][col]
     }
 
-    pub fn get_mut(&mut self, i: usize, j: usize) -> &mut T {
-        &mut self.values[i][j]
+    pub fn get_mut(&mut self, row: usize, col: usize) -> &mut T {
+        &mut self.values[row][col]
     }
 
     pub fn enumerate(&self) -> impl Iterator<Item = (usize, usize, &T)> {
-        self.values
-            .iter()
-            .enumerate()
-            .flat_map(|(i, v)| v.iter().enumerate().map(move |(j, c)| (i, j, c)))
+        self.values.iter().enumerate().flat_map(|(row, v)| {
+            v.iter()
+                .enumerate()
+                .map(move |(col, value)| (row, col, value))
+        })
     }
 
     pub fn enumerate_mut(&mut self) -> impl Iterator<Item = (usize, usize, &mut T)> {
-        self.values
-            .iter_mut()
-            .enumerate()
-            .flat_map(|(i, v)| v.iter_mut().enumerate().map(move |(j, c)| (i, j, c)))
+        self.values.iter_mut().enumerate().flat_map(|(row, v)| {
+            v.iter_mut()
+                .enumerate()
+                .map(move |(col, value)| (row, col, value))
+        })
     }
 
     pub fn rows(&self) -> usize {
@@ -61,7 +105,7 @@ impl<T> Grid<T> {
         (0..self.cols()).map(move |c| (0..self.rows()).map(move |r| self.get(r, c)))
     }
 
-    pub fn insert_row_at(&mut self, i: usize, values: impl IntoIterator<Item = T>) {
+    pub fn insert_row_at(&mut self, row: usize, values: impl IntoIterator<Item = T>) {
         let new_row: Vec<_> = values.into_iter().take(self.cols()).collect();
 
         if new_row.len() != self.cols() {
@@ -72,10 +116,10 @@ impl<T> Grid<T> {
             )
         }
 
-        self.values.insert(i, new_row);
+        self.values.insert(row, new_row);
     }
 
-    pub fn insert_col_at(&mut self, i: usize, values: impl IntoIterator<Item = T>) {
+    pub fn insert_col_at(&mut self, col: usize, values: impl IntoIterator<Item = T>) {
         let mut new_col: Vec<_> = values.into_iter().take(self.rows()).collect();
 
         if new_col.len() != self.rows() {
@@ -89,22 +133,34 @@ impl<T> Grid<T> {
         self.values
             .iter_mut()
             .rev()
-            .for_each(|v| v.insert(i, new_col.pop().unwrap()));
+            .for_each(|v| v.insert(col, new_col.pop().unwrap()));
+    }
+
+    pub fn position<U: TryInto<usize>>(&self, row: U, col: U) -> Option<Position<T>> {
+        match (row.try_into(), col.try_into()) {
+            (Ok(r), Ok(c)) if r < self.rows() && c < self.cols() => Some(Position {
+                row: r,
+                col: c,
+                grid: self,
+            }),
+            _ => None,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
-    fn create_test_grid() -> Grid<char> {
+    #[fixture]
+    fn grid() -> Grid<char> {
         let lines = vec!["abc", "def", "ghi", "jkl"];
         lines.iter().map(|l| l.chars()).collect()
     }
 
-    #[test]
-    fn test_direct_values() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_direct_values(grid: Grid<char>) {
         assert!(!grid.values.is_empty());
         assert_eq!(grid.values.len(), 4);
         assert_eq!(grid.values[0].len(), 3);
@@ -113,15 +169,13 @@ mod tests {
         assert_eq!(grid.values[1][1], 'e');
     }
 
-    #[test]
-    fn test_get_by_index() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_get_by_index(grid: Grid<char>) {
         assert_eq!(*grid.get(1, 1), 'e');
     }
 
-    #[test]
-    fn test_enumerate() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_enumerate(grid: Grid<char>) {
         let positions: Vec<_> = grid.enumerate().map(|(a, b, c)| (a, b, *c)).collect();
 
         assert_eq!(positions[0], (0, 0, 'a'));
@@ -129,54 +183,105 @@ mod tests {
         assert_eq!(positions[4], (1, 1, 'e'));
     }
 
-    #[test]
-    fn test_rows() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_rows(grid: Grid<char>) {
         assert_eq!(grid.rows(), 4);
     }
 
-    #[test]
-    fn test_cols() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_cols(grid: Grid<char>) {
         assert_eq!(grid.cols(), 3);
     }
 
-    #[test]
+    #[rstest]
     #[should_panic]
     fn test_invalid_collect() {
         let lines = vec!["abc", "defg"];
         let _: Grid<_> = lines.iter().map(|l| l.chars()).collect();
     }
 
-    #[test]
-    fn test_row_wise_iter() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_row_wise_iter(grid: Grid<char>) {
         let row: Vec<_> = grid.row_wise_iter().next().unwrap().map(|c| *c).collect();
 
         assert_eq!(row, vec!['a', 'b', 'c']);
     }
 
-    #[test]
-    fn test_col_wise_iter() {
-        let grid = create_test_grid();
+    #[rstest]
+    fn test_col_wise_iter(grid: Grid<char>) {
         let col: Vec<_> = grid.col_wise_iter().next().unwrap().map(|c| *c).collect();
 
         assert_eq!(col, vec!['a', 'd', 'g', 'j']);
     }
 
-    #[test]
-    fn test_insert_row_at() {
-        let mut grid = create_test_grid();
+    #[rstest]
+    fn test_insert_row_at(mut grid: Grid<char>) {
         grid.insert_row_at(1, "123".chars());
 
         assert_eq!(*grid.get(1, 0), '1');
     }
 
-    #[test]
-    fn test_insert_col_at() {
-        let mut grid = create_test_grid();
+    #[rstest]
+    fn test_insert_col_at(mut grid: Grid<char>) {
         grid.insert_col_at(1, "1234".chars());
 
         assert_eq!(*grid.get(0, 1), '1');
+    }
+
+    #[rstest]
+    #[case(-1, 0)]
+    #[case(0, -1)]
+    #[case(4, 0)]
+    #[case(0, 3)]
+    fn test_invalid_positions(grid: Grid<char>, #[case] row: i32, #[case] col: i32) {
+        assert!(grid.position(row, col).is_none());
+    }
+
+    #[rstest]
+    #[case(0, 0, 'a')]
+    #[case(1, 1, 'e')]
+    #[case(3, 0, 'j')]
+    fn test_valid_positions(
+        grid: Grid<char>,
+        #[case] row: i32,
+        #[case] col: i32,
+        #[case] value: char,
+    ) {
+        let pos = grid.position(row, col).unwrap();
+        assert_eq!(row as usize, pos.row());
+        assert_eq!(col as usize, pos.col());
+        assert_eq!(value, *pos.value());
+    }
+
+    #[rstest]
+    #[case(0, 0, Direction::North)]
+    #[case(0, 0, Direction::West)]
+    #[case(3, 2, Direction::East)]
+    #[case(3, 2, Direction::South)]
+    fn test_neighbors_invalid_positions(
+        grid: Grid<char>,
+        #[case] row: usize,
+        #[case] col: usize,
+        #[case] dir: Direction,
+    ) {
+        let pos = grid.position(row, col).unwrap();
+        assert!(pos.get_neighbor(dir).is_none())
+    }
+
+    #[rstest]
+    #[case(1, 1, Direction::North, 'b')]
+    #[case(1, 1, Direction::East, 'f')]
+    #[case(1, 1, Direction::South, 'h')]
+    #[case(1, 1, Direction::West, 'd')]
+    fn test_neighbors_valid_positions(
+        grid: Grid<char>,
+        #[case] row: usize,
+        #[case] col: usize,
+        #[case] dir: Direction,
+        #[case] value: char,
+    ) {
+        let pos = grid.position(row, col).unwrap();
+        let neighbor = pos.get_neighbor(dir).unwrap();
+        assert_eq!(value, *neighbor.value());
     }
 }
