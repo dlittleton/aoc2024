a5ea512ebe6310002700e1298d563e443f1d7a19
Duane Littleton <dlittleton87@gmail.com>
2024-12-14 11:51:46 -0500

    Day 14, part 2

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -1,8 +1,10 @@
-use aoc2024::input::get_all_numbers;
-use tracing::info;
+use std::iter;
+
+use aoc2024::{collections::grid::Grid, input::get_all_numbers};
+use tracing::{debug, info};
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
 }
 
 struct Robot {
@@ -33,6 +35,103 @@ impl Robot {
 
         (px, py)
     }
+
+    fn advance(&mut self, width: i32, height: i32) {
+        self.px = (self.px + self.vx).rem_euclid(width);
+        self.py = (self.py + self.vy).rem_euclid(height);
+    }
+}
+
+struct RoboGrid {
+    robots: Vec<Robot>,
+    grid: Grid<i32>,
+}
+
+impl RoboGrid {
+    fn new(robots: Vec<Robot>, width: i32, height: i32) -> Self {
+        let mut grid: Grid<i32> = iter::repeat(iter::repeat(0).take(width as usize))
+            .take(height as usize)
+            .collect();
+
+        info!(
+            "Created grid with {} rows and {} cols",
+            grid.rows(),
+            grid.cols()
+        );
+
+        for bot in robots.iter() {
+            *grid.get_mut(bot.py as usize, bot.px as usize) += 1;
+        }
+
+        Self { robots, grid }
+    }
+
+    fn advance(&mut self) {
+        for bot in self.robots.iter_mut() {
+            *self.grid.get_mut(bot.py as usize, bot.px as usize) -= 1;
+            bot.advance(self.grid.cols() as i32, self.grid.rows() as i32);
+            *self.grid.get_mut(bot.py as usize, bot.px as usize) += 1;
+        }
+    }
+
+    fn dump_grid(&self) {
+        for row in self.grid.row_wise_iter() {
+            let line: String = row.map(|v| if *v > 0 { "+" } else { "." }).collect();
+            println!("{}", line)
+        }
+    }
+
+    fn detect_tree(&self) -> usize {
+        // Note: The assumed shape turned out to be wildly incorrect, but in the
+        // process of trying to find heuristics the below check for contiguous
+        // rows seems to be good enough.
+
+        // Assumed tree shape
+        // ....+....
+        // ...+.+...
+        // ..+...+..
+        // .+++++++.
+        // ....+....
+
+        // Heuristic: Search for the bottom line.
+        let mut longest_row = 0;
+
+        for row in self.grid.row_wise_iter() {
+            let mut contiguous = 0;
+            for col in row {
+                if *col > 0 {
+                    contiguous += 1;
+                } else {
+                    longest_row = longest_row.max(contiguous);
+                    contiguous = 0;
+                }
+            }
+            longest_row = longest_row.max(contiguous);
+        }
+
+        longest_row as usize
+    }
+
+    fn search_tree(&mut self) -> usize {
+        let mut t = 0;
+
+        self.dump_grid();
+        loop {
+            t += 1;
+            self.advance();
+            //self.dump_grid();
+            let bots_in_tree = self.detect_tree();
+            debug!("Time {} - Bots in tree {}", t, bots_in_tree);
+
+            if bots_in_tree > 10 {
+                info!("There are {} bots in the tree. Stopping.", bots_in_tree);
+                self.dump_grid();
+                break;
+            }
+        }
+
+        t
+    }
 }
 
 fn solve(input: &str, width: i32, height: i32) -> String {
@@ -62,8 +161,21 @@ fn solve(input: &str, width: i32, height: i32) -> String {
     total.to_string()
 }
 
+fn solve2(input: &str, width: i32, height: i32) -> String {
+    let bots: Vec<_> = input.lines().map(Robot::parse).collect();
+    let mut robogrid = RoboGrid::new(bots, width, height);
+
+    let t = robogrid.search_tree();
+
+    t.to_string()
+}
+
 fn part1(input: &str) -> String {
     solve(input, 101, 103)
+}
+
+fn part2(input: &str) -> String {
+    solve2(input, 101, 103)
 }
 
 #[cfg(test)]
@@ -90,6 +202,6 @@ mod wrapped_sample_test {
     p=7,3 v=-1,2
     p=2,4 v=2,-3
     p=9,5 v=-3,-3",
-        part1_test_size = "12"
+        part1_test_size = "12",
     }
 }
