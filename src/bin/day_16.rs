f2ee5543cea877afb78488c92ade865490b68367
Duane Littleton <dlittleton87@gmail.com>
2024-12-16 09:40:39 -0500

    Day 16, part 2

--- src/bin/day_16.rs
+++ src/bin/day_16.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashSet};
 
 use aoc2024::{
     collections::grid::{Direction, Grid, Position},
@@ -7,7 +7,7 @@ use aoc2024::{
 use tracing::{debug, info};
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
 }
 
 type Point = (usize, usize);
@@ -16,11 +16,40 @@ struct Path<'a> {
     score: usize,
     pos: Position<'a, char>,
     dir: Direction,
+    nodes: Vec<Point>,
 }
 
 impl<'a> Path<'a> {
     fn new(score: usize, pos: Position<'a, char>, dir: Direction) -> Self {
-        Self { score, pos, dir }
+        let nodes = vec![(pos.row(), pos.col())];
+        Self {
+            score,
+            pos,
+            dir,
+            nodes,
+        }
+    }
+
+    fn step(&self) -> Option<Self> {
+        self.pos.get_neighbor(self.dir).map(|p| {
+            let mut nodes = self.nodes.clone();
+            nodes.push((p.row(), p.col()));
+            Self {
+                score: self.score + 1,
+                pos: p,
+                dir: self.dir,
+                nodes,
+            }
+        })
+    }
+
+    fn turn(&self, dir: Direction) -> Self {
+        Self {
+            score: self.score + 1000,
+            pos: self.pos,
+            dir,
+            nodes: self.nodes.clone(),
+        }
     }
 }
 
@@ -75,13 +104,16 @@ impl Maze {
         }
     }
 
-    fn solve(&self) -> Option<usize> {
+    fn solve(&self) -> (usize, Vec<Vec<Point>>) {
         let start_pos = self.grid.position(self.start.0, self.start.1).unwrap();
         let (r_end, c_end) = self.end;
 
         let mut best = self
             .grid
             .map(|_| [usize::MAX, usize::MAX, usize::MAX, usize::MAX]);
+
+        let mut low_score = None;
+        let mut paths = Vec::new();
 
         let mut heap = BinaryHeap::new();
         heap.push(Path::new(0, start_pos, Direction::East));
@@ -94,8 +126,14 @@ impl Maze {
                 path.score
             );
 
+            if let Some(low) = low_score {
+                if path.score > low {
+                    continue;
+                }
+            }
+
             let best_for_pos = &mut best.get_mut(path.pos.row(), path.pos.col())[path.dir as usize];
-            if path.score >= *best_for_pos {
+            if path.score > *best_for_pos {
                 info!(
                     "Pruning position ({}, {}) due to lower score.",
                     path.pos.row(),
@@ -106,30 +144,43 @@ impl Maze {
             *best_for_pos = path.score;
 
             if path.pos.row() == r_end && path.pos.col() == c_end {
-                return Some(path.score);
+                low_score = Some(path.score);
+                paths.push(path.nodes);
+                continue;
             }
 
             // Step forward
-            if let Some(step) = path.pos.get_neighbor(path.dir) {
-                if *step.value() != '#' {
-                    heap.push(Path::new(path.score + 1, step, path.dir))
+            if let Some(step) = path.step() {
+                if *step.pos.value() != '#' {
+                    heap.push(step);
                 }
             }
 
             // Turn
             for turn in Self::get_turn_dirs(path.dir) {
-                heap.push(Path::new(path.score + 1000, path.pos, turn))
+                heap.push(path.turn(turn));
             }
         }
 
-        None
+        (low_score.unwrap(), paths)
     }
 }
 
 fn part1(input: &str) -> String {
     let maze = Maze::parse(input);
-    let score = maze.solve().unwrap();
+    let score = maze.solve().0;
     score.to_string()
+}
+
+fn part2(input: &str) -> String {
+    let maze = Maze::parse(input);
+    let paths = maze.solve().1;
+
+    info!("There are {} paths.", paths.len());
+
+    let unique: HashSet<Point> = paths.iter().flat_map(|p| p.iter()).copied().collect();
+
+    unique.len().to_string()
 }
 
 sample! {
@@ -149,5 +200,6 @@ sample! {
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############",
-    part1 = "7036"
+    part1 = "7036",
+    part2 = "45"
 }
