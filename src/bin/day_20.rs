80d548915ab1c20e3a104fcfdebf35578c145009
Duane Littleton <dlittleton87@gmail.com>
2024-12-21 09:53:20 -0500

    Day 20, part 2

--- src/bin/day_20.rs
+++ src/bin/day_20.rs
@@ -1,13 +1,23 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 
 use aoc2024::collections::grid::{Grid, Position, CARDINAL_DIRECTIONS};
-use tracing::info;
+use tracing::{debug, info};
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
 }
 
 type Point = (usize, usize);
+
+fn distance(a: &Point, b: &Point) -> usize {
+    a.0.abs_diff(b.0) + a.1.abs_diff(b.1)
+}
+
+struct PathNode {
+    point: Point,
+    d_start: usize,
+    d_end: usize,
+}
 
 struct Maze {
     grid: Grid<char>,
@@ -25,42 +35,60 @@ impl Maze {
         Self { grid, start, end }
     }
 
-    fn start_pos(&self) -> Position<char> {
-        self.grid.position(self.start.0, self.start.1).unwrap()
+    fn end_pos(&self) -> Position<char> {
+        self.grid.position(self.end.0, self.end.1).unwrap()
     }
 
-    fn search(&self, skip: Option<Point>) -> usize {
-        let mut seen: HashSet<Point> = HashSet::new();
-        let mut to_visit = VecDeque::new();
+    fn distance_from_end(&self) -> (usize, Vec<PathNode>) {
+        let mut lengths = self.grid.map(|_| 0);
 
-        to_visit.push_back((self.start_pos(), 0));
+        let mut to_visit = VecDeque::new();
+        to_visit.push_back((self.end_pos(), 0));
 
         while let Some((pos, depth)) = to_visit.pop_front() {
-            if self.end == pos.into() {
-                return depth;
-            }
+            *lengths.get_mut(pos.row(), pos.col()) = depth;
 
-            if seen.contains(&pos.into()) {
-                continue;
-            }
-
-            seen.insert(pos.into());
             for n in pos.get_neighbors(&CARDINAL_DIRECTIONS) {
-                if *n.value() != '#' || skip.is_some_and(|v| v == n.into()) {
+                if !matches!(n.value(), '#' | 'E') && *lengths.get(n.row(), n.col()) == 0 {
                     to_visit.push_back((n, depth + 1));
                 }
             }
         }
 
-        panic!("Unable to reach exit.");
+        let origin = *lengths.get(self.start.0, self.start.1);
+
+        let nodes = self
+            .grid
+            .enumerate()
+            .filter_map(|(r, c, v)| match v {
+                '#' => None,
+                _ => Some(PathNode {
+                    point: (r, c),
+                    d_start: origin - lengths.get(r, c),
+                    d_end: *lengths.get(r, c),
+                }),
+            })
+            .collect();
+
+        (origin, nodes)
     }
 
-    fn try_cheats(&self) -> Vec<usize> {
+    fn find_cheats(&self, max_cheat: usize) -> Vec<usize> {
+        let (origin, nodes) = self.distance_from_end();
         let mut result = Vec::new();
 
-        for (r, c, v) in self.grid.enumerate() {
-            if matches!(v, '#') {
-                result.push(self.search(Some((r, c))));
+        for a in nodes.iter() {
+            for b in nodes.iter() {
+                let d = distance(&a.point, &b.point);
+                // Only cheat between nodes if the target node is closer to the end.
+                if d > 0 && d <= max_cheat && b.d_end < a.d_end {
+                    let total_distance = a.d_start + d + b.d_end;
+                    debug!(
+                        "Path from ({}, {}) to ({}, {}) is length {}",
+                        a.point.0, a.point.1, b.point.0, b.point.1, total_distance
+                    );
+                    result.push(origin - total_distance);
+                }
             }
         }
 
@@ -68,23 +96,22 @@ impl Maze {
     }
 }
 
-fn solve(input: &str, limit: usize) -> String {
+fn solve(input: &str, max_cheat: usize, limit: usize) -> String {
     let maze = Maze::parse(input);
 
-    let original = maze.search(None);
-    info!("Original path length: {}", original);
-
-    let paths = maze.try_cheats();
+    let paths = maze.find_cheats(max_cheat);
     info!("There are {} cheat paths.", paths.len());
+    let count = paths.iter().filter(|v| **v >= limit).count();
 
-    let good = paths.iter().filter(|v| (original - **v) >= limit).count();
-    info!("There are {} paths that save at least {}", good, limit);
-
-    good.to_string()
+    count.to_string()
 }
 
 fn part1(input: &str) -> String {
-    solve(input, 100)
+    solve(input, 2, 100)
+}
+
+fn part2(input: &str) -> String {
+    solve(input, 20, 100)
 }
 
 #[cfg(test)]
@@ -94,9 +121,13 @@ mod test {
 
     use super::*;
 
-    fn test_part_1(input: &str) -> String {
+    fn test_part1(input: &str) -> String {
         // Use shorter cutoff for the sample maze
-        solve(input, 20)
+        solve(input, 2, 20)
+    }
+
+    fn test_part2(input: &str) -> String {
+        solve(input, 20, 70)
     }
 
     sample! {
@@ -116,6 +147,7 @@ mod test {
 #.#.#.#.#.#.###
 #...#...#...###
 ###############",
-        test_part_1 = "5"
+        test_part1 = "5",
+        test_part2 = "41"
     }
 }
