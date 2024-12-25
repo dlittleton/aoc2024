5b69edc34be8562072ab4be9344bb0ef335f574e
Duane Littleton <dlittleton87@gmail.com>
2024-12-24 21:05:26 -0500

    Part two solved for test input. Too slow for real input.

--- src/bin/day_24.rs
+++ src/bin/day_24.rs
@@ -1,9 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+use rayon::prelude::*;
 
 use aoc2024::{input::get_all_numbers, sample};
+use itertools::Itertools;
+use tracing::{debug, info};
 
 fn main() {
-    aoc2024::run(part1, None);
+    aoc2024::run(part1, Some(part2));
 }
 
 #[derive(Clone, Copy)]
@@ -36,9 +40,11 @@ struct Node<'a> {
     op: Operation,
 }
 
+#[derive(Clone)]
 struct Device<'a> {
     x: u64,
     y: u64,
+    input_bits: u64,
     nodes: HashMap<&'a str, Node<'a>>,
     output_keys: Vec<&'a str>,
 }
@@ -49,12 +55,15 @@ impl<'a> Device<'a> {
 
         let mut x = 0;
         let mut y = 0;
+        let mut input_bits = 0;
 
         for (c, shift, value) in lines
             .by_ref()
             .take_while(|l| !l.is_empty())
             .map(Self::parse_initial)
         {
+            input_bits = input_bits.max(shift);
+
             match c {
                 'x' => x |= value << shift,
                 'y' => y |= value << shift,
@@ -76,6 +85,7 @@ impl<'a> Device<'a> {
         Self {
             x,
             y,
+            input_bits,
             nodes,
             output_keys,
         }
@@ -144,6 +154,82 @@ impl<'a> Device<'a> {
         }
         acc
     }
+
+    fn swap(&mut self, n1: &'a str, n2: &'a str) {
+        let temp = *self.nodes.get(n1).unwrap();
+        self.nodes.insert(n1, *self.nodes.get(n2).unwrap());
+        self.nodes.insert(n2, temp);
+    }
+
+    fn set_inputs(&mut self, x: u64, y: u64) {
+        self.x = x;
+        self.y = y;
+    }
+}
+
+fn solve2(input: &str, swaps: usize, combine: fn(u64, u64) -> u64) -> String {
+    let device = Device::parse(input);
+
+    let max = (1 << (device.input_bits + 1)) - 1;
+
+    let mut test_cases = vec![(0, 0), (max, max)];
+    for i in 0..device.input_bits {
+        let val = 1 << i;
+        test_cases.push((val, val));
+        test_cases.push((val, 0));
+        test_cases.push((0, val));
+    }
+
+    let nodes: Vec<_> = device.nodes.keys().copied().collect();
+    let pairs: Vec<(&str, &str)> = nodes.iter().copied().tuple_combinations().collect();
+
+    let result: Vec<_> = pairs
+        .iter()
+        .combinations(swaps)
+        .par_bridge()
+        .filter(|swapset| {
+            let mut cloned_device = device.clone();
+
+            let mut seen = HashSet::new();
+            for (n1, n2) in swapset {
+                if seen.contains(n1) || seen.contains(n2) {
+                    debug!("Skipping swapset due to duplicate node: {:?}", swapset);
+                    return false;
+                }
+                cloned_device.swap(n1, n2);
+                seen.insert(n1);
+                seen.insert(n2);
+            }
+
+            let result = test_cases.iter().all(|(x, y)| {
+                let expected = combine(*x, *y);
+                cloned_device.set_inputs(*x, *y);
+
+                let actual = cloned_device.get_output();
+                expected == actual
+            });
+
+            if result {
+                info!("Found potential solution. {:?}", swapset);
+            }
+
+            result
+        })
+        .collect();
+
+    if result.len() != 1 {
+        panic!("Bad result count. {:?}", result);
+    }
+
+    let mut nodes = Vec::new();
+    let swaps = result.first().unwrap();
+    for (n1, n2) in swaps {
+        nodes.push(n1);
+        nodes.push(n2);
+    }
+
+    nodes.sort();
+    nodes.iter().join(",")
 }
 
 fn part1(input: &str) -> String {
@@ -151,6 +237,10 @@ fn part1(input: &str) -> String {
     let value = device.get_output();
 
     value.to_string()
+}
+
+fn part2(input: &str) -> String {
+    solve2(input, 4, |a, b| a + b)
 }
 
 sample! {
@@ -203,4 +293,37 @@ hwm AND bqk -> z03
 tgd XOR rvg -> z12
 tnw OR pbm -> gnj",
     part1 = "2024"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test_part2(input: &str) -> String {
+        solve2(input, 2, |a, b| a & b)
+    }
+
+    sample! {
+        r"
+x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
+
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00",
+        test_part2 = "z00,z01,z02,z05"
+    }
 }
